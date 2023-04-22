c8d3add5cd69149e00c82f334678b1daa44d36d2
qaws7791 <qaws7791@gmail.com>
2023-04-23 00:38:37 +0900

    빨간 상자가 화면에 보이도록 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,102 +1,187 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+"use client"
+import { Wrapper } from "@googlemaps/react-wrapper";
+import { ThreeJSOverlayView } from "@googlemaps/three";
+import { useRef,useEffect, useState } from 'react';
+import {CatmullRomCurve3,MeshBasicMaterial} from 'three';
+import {Line2} from 'three/examples/jsm/lines/Line2.js';
+import {LineGeometry} from 'three/examples/jsm/lines/LineGeometry.js';
+import {LineMaterial} from 'three/examples/jsm/lines/LineMaterial.js';
+import { getGeocode, getLatLng } from "use-places-autocomplete";
+import { BoxGeometry, MathUtils, Mesh, MeshMatcapMaterial } from "three";
 
-const inter = Inter({ subsets: ['latin'] })
+const mapOptions = {
+  mapId: '5f9c1c0db664c52d',
+  center: {lng: -122.343787,
+    lat: 47.607465},
+  zoom: 15,
+  heading: 45,
+  tilt: 67,
+}
+
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <Wrapper apiKey={'AIzaSyAL-RpPj9UzyX_qAUMANn1-wJyUj5atXQc'}>
+    <MapComponent />
+  </Wrapper>
   )
 }
+
+function MapComponent() {
+  const [map,setMap] = useState();
+  const ref = useRef();
+  const overlay = useRef();
+
+    useEffect(() => {
+    setMap(new window.google.maps.Map(ref.current, mapOptions));
+    
+  },[]);
+
+  useEffect(() => {
+    if(map) {
+      if(!overlay.current) {
+        overlay.current = new ThreeJSOverlayView({
+          map
+        })
+      }
+  
+      const box = new Mesh(
+        new BoxGeometry(100, 500, 100),
+        new MeshBasicMaterial({color: 0xff0000})
+      );
+      const pos = overlay.current.latLngAltitudeToVector3(mapOptions.center);
+      box.position.copy(pos);
+      console.log(pos)
+      console.log(map)
+      console.log(overlay.current)
+      console.log(overlay.current.latLngAltitudeToVector3(mapOptions.center))
+      // add box mesh to the scene
+      overlay.current.scene.add(box);
+      console.log(box)
+  
+      const animate = () => {
+        box.rotateY(MathUtils.degToRad(0.1));
+        requestAnimationFrame(animate);
+      };
+      requestAnimationFrame(animate);
+    }
+  },[map])
+
+  return (<div id="map" ref={ref}></div>)
+}
+
+
+// function MyMapComponent() {
+//   const [map,setMap] = useState();
+//   const [route,setRoute] = useState()
+//   const ref = useRef();
+
+//   useEffect(() => {
+//     setMap(new window.google.maps.Map(ref.current, mapOptions));
+//   },[]);
+
+//   return (
+//     <div className="main">
+//       <div ref={ref} id="map" />
+//       {map && <Direction setRoute={setRoute}/>}
+//       {map && route && <Animation map={map} route={route}/>}
+//     </div>
+//   );
+// }
+
+// function Direction({setRoute}) {
+//   const [origin] = useState('27 Front St Toronto')
+//   const [destination] = useState('75 Yonge Street Toronto')
+
+//   useEffect(() => {
+//     fetchDirections(origin, destination,setRoute)
+//   },[origin,destination])
+
+//   return (<div className="directions">
+//   <h2>Directions</h2>
+//   <h3>Origin</h3>
+//   <p>{origin}</p>
+//   <h3>Destination</h3>
+//   <p>{destination}</p>
+// </div>)
+    
+// }
+
+// async function  fetchDirections(origin, destination, setRoute) {
+//   const [ originResults, destinationResults ] = await Promise.all([
+//     getGeocode({address: origin}),
+//     getGeocode({address: destination})
+//   ])
+
+//   const [originLoc, destinationLoc] = await Promise.all([
+//     getLatLng(originResults[0]),
+//     getLatLng(destinationResults[0])
+//   ]);
+//   console.log(originLoc,destinationLoc)
+
+//   const service = new google.maps.DirectionsService();
+//   service.route({
+//     origin: originLoc,
+//     destination: destinationLoc,
+//     travelMode: google.maps.TravelMode.DRIVING,
+//   },
+//   (result,status) => {
+//     if(status === 'OK' && result) {
+//       const route = result.routes[0].overview_path.map(path=> ({
+//         lat: path.lat(),
+//         lng: path.lng()
+//       }))
+//       setRoute(route)
+//       console.log(route)
+//     }
+//   })
+// }
+
+
+// function Animation({map, route}){
+//   const overlayRef = useRef();
+//   const trackRef = useRef();
+
+//   useEffect(() => {
+//     map.setCenter(route[Math.floor(route.length/2)],17)
+
+//     if(!overlayRef.current) {
+//       overlayRef.current = new ThreeJSOverlayView({anchor: mapOptions.center})
+//       overlayRef.current.setMap(map);
+//     }
+
+//     const scene = overlayRef.current.scene;
+//     const points = route.map((p) => overlayRef.current.latLngAltitudeToVector3(p))
+//     console.log(points)
+//     const curve = new CatmullRomCurve3(points);
+//     if(trackRef.current) {
+//       scene.remove(trackRef.current)
+//     }
+//     trackRef.current = createTrackFromCurve(curve)
+//     console.log(curve)
+//     scene.add(trackRef.current);
+
+//     overlayRef.current.update = () => {
+//       overlayRef.current.getViewportSize()
+//     }
+
+//     overlayRef.current.requestRedraw();
+//   },[route])
+
+//   return null;
+// }
+
+// function createTrackFromCurve(curve) {
+//   const points = curve.getSpacedPoints(curve.points.length * 10)
+//   const positions = points.map((point) => point.toArray()).flat();
+//   console.log(points)
+//   return new Line2(
+//     new LineGeometry().setPositions(positions),
+//     new LineMaterial({
+//       color: 0xffb703,
+//       linewidth: 8,
+//     })
+//   )
+  
+
+// }
