8ece8edb990bc829e6f24e22735a82052ac3c8f1
qaws7791 <qaws7791@gmail.com>
2023-04-23 02:24:25 +0900

    api 키를 제거하고 보이지 않도록 환경변수에 저장

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import { getGeocode, getLatLng } from "use-places-autocomplete";
 import { BoxGeometry, MathUtils, Mesh, MeshMatcapMaterial } from "three";
 
 const mapOptions = {
-  mapId: '5f9c1c0db664c52d',
+  mapId: process.env.NEXT_PUBLIC_MAP_ID,
   center: {lng: -122.343787,
     lat: 47.607465},
   zoom: 15,
@@ -20,7 +20,7 @@ const mapOptions = {
 
 export default function Home() {
   return (
-    <Wrapper apiKey={'AIzaSyAL-RpPj9UzyX_qAUMANn1-wJyUj5atXQc'}>
+    <Wrapper apiKey={process.env.NEXT_PUBLIC_MAP_API_KEY}>
     <MapComponent />
   </Wrapper>
   )
@@ -34,12 +34,21 @@ function MapComponent() {
 
     useEffect(() => {
     setMap(new window.google.maps.Map(ref.current, mapOptions));
+    setRoute([
+      {lng:127.11227377269273,lat:37.39472345624831},
+      {lng:127.11273699016535,lat:37.39470891137642},
+      {lng:127.11275943405961,lat:37.39376294953177},
+      {lng:127.10966524621578,lat:37.3937396926337},
+      {lng:127.10967214334856,lat:37.399840028043634},
+      {lng:127.10374249170327,lat:37.39984028415526},
+      {lng:127.10371917998,lat:37.400849311369676},
+      ])
     
   },[]);
 
   return (<div>
     <div id="map" ref={ref}></div>
-    {map && <Direction setRoute={setRoute}/>}
+    {/* {map && <Direction setRoute={setRoute}/>} */}
     {map && route && <Animation map={map} route={route} mapref={ref}/>}
     </div>)
 }
@@ -59,7 +68,7 @@ function Animation({map,route,mapref}) {
     console.log(route)
     const points = route.map((p) => overlay.current.latLngAltitudeToVector3(p))
     console.log(points)
-    const curve = new CatmullRomCurve3(points);
+    const curve = new CatmullRomCurve3(points,false,'catmullrom',0.1);
     if(trackRef.current) {
       overlay.current.scene.remove(trackRef.current)
     }
@@ -203,14 +212,14 @@ async function  fetchDirections(origin, destination, setRoute) {
 // }
 
 function createTrackFromCurve(curve) {
-  const points = curve.getSpacedPoints(curve.points.length * 10);
+  const points = curve.getSpacedPoints(curve.points.length * 50);
   const positions = points.map((point) => point.toArray()).flat();
 
   return new Line2(
     new LineGeometry().setPositions(positions),
     new LineMaterial({
-      color: 0xffb703,
-      linewidth: 8,
+      color: 0xff0000,
+      linewidth: 6,
     })
   );
 }
